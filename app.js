81788d2df11d37b048049956aab3363bcf37481c
thisisrid <myworld.shoyab@gmail.com>
2021-02-13 19:35:53 +0600

    enter key option added

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 var lyricArea = document.getElementById('lyricArea');
 var suggContainer = document.getElementById('suggContainer');
 
+const showSuggesion = () => {
+    const getInput = document.getElementById('inputForm').value;
+    if (getInput != '') {
+        GetSearched(getInput);
+    }
+    else {
+        alert("GIVE A VALID NAME")
+    }
+}
+
 const GetSearched = async(searchText)=>{
     toggleSpinner();
     const url = `https://api.lyrics.ovh/suggest/${searchText}`
@@ -11,29 +21,6 @@ const GetSearched = async(searchText)=>{
     toggleSpinner();
     
 }
-
-const GetLyrics = async(artist, title)=>{
-    
-    LyricsSpinner();
-    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
-    const res = await fetch(url);
-    const data = await res.json();
-    
-        if (data.lyrics != '') {
-            lyricArea.innerText = data.lyrics;
-            LyricsSpinner();
-        }
-    else {
-        lyricArea.innerText = 'Sorry, Lyrics Not Found! Try With Another Song!';
-        LyricsSpinner();
-    }
-}
-
-const showSuggesion = () => {
-    const getInput = document.getElementById('inputForm').value;
-    GetSearched(getInput);
-}
-
 const display = (arrayName) => {
 
     suggContainer.style.display = "block";
@@ -62,8 +49,28 @@ newDiv.className = "single-result row align-items-center my-3 p-3"
         `
         suggContainer.appendChild(newDiv);
     });
-
 } 
+
+const GetLyrics = async(artist, title)=>{
+    
+    LyricsSpinner();
+    const url = `https://api.lyrics.ovh/v1/${artist}/${title}`
+    const res = await fetch(url);
+    const data = await res.json();
+    
+        if (data.lyrics != '') {
+            lyricArea.innerText = data.lyrics;
+            LyricsSpinner();
+        }
+    else {
+        lyricArea.innerText = 'Sorry, Lyrics Not Found! Try With Another Song!';
+        LyricsSpinner();
+    }
+}
+
+
+
+
 const toggleSpinner = () => {
     const spinner = document.getElementById('spinner');
     const suggContainer = document.getElementById('suggContainer');
@@ -76,5 +83,12 @@ const LyricsSpinner = () => {
     spinner.classList.toggle('d-none');
     lyricArea.classList.toggle('d-none');
 }
+function handle(e){
+    if(e.key === "Enter"){
+showSuggesion();
+        
+    }
 
+    return false;
+}
 
